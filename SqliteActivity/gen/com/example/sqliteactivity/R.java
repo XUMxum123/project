0ae1e2ffeee406f9fe8d0d4ae67d058f95f6c336
toby <toby.xu@tpv-tech.com>
2016-10-20 16:37:05 +0800

    autogeneration

--- SqliteActivity/gen/com/example/sqliteactivity/R.java
+++ SqliteActivity/gen/com/example/sqliteactivity/R.java
@@ -24,11 +24,15 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
-        public static final int listView=0x7f080000;
+        public static final int action_settings=0x7f080004;
+        public static final int listPerson=0x7f080001;
+        public static final int listView=0x7f080003;
+        public static final int personQuery=0x7f080000;
+        public static final int proivide_before_click_add=0x7f080002;
     }
     public static final class layout {
-        public static final int activity_sqlite=0x7f030000;
+        public static final int activity_personresolver=0x7f030000;
+        public static final int activity_sqlite=0x7f030001;
     }
     public static final class menu {
         public static final int sqlite=0x7f070000;
@@ -39,6 +43,8 @@ public final class R {
         public static final int app_name=0x7f050000;
         public static final int delete=0x7f050005;
         public static final int hello_world=0x7f050002;
+        public static final int person_query=0x7f050009;
+        public static final int proivide_before_click_add=0x7f050008;
         public static final int query=0x7f050006;
         public static final int queryTheCursor=0x7f050007;
         public static final int update=0x7f050004;
